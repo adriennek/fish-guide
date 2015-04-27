5e721dd6b5997d544312972509546fadd3cbb5ac
Rufasa85 <joe.rehfuss@gmail.com>
2015-04-27 16:58:14 -0700

    added default image if no image selected, and some general styling

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ActiveRecord::Base
 	validates :name, :description, presence: true
+	FAMILIES= ['Roundfish', 'Flatfish', 'Rockfish', 'Mammal', 'Salmon', 'Skate', 'Sculpin', 'Incidental', 'Invertebrate']
+	
 end
